60d1215ec2820fb9846a2b06df435ceeda0da092
SBriscoe2 <shabris@tiscali.co.uk>
2018-12-03 16:06:31 +0000

    added feature file and rspec folders

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,16 @@
 require 'pg'
-
 class User
+
+attr_reader :username, :password, :email,
+
+  def initialize (username, password, email)
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  def create_account
+
+  end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,7 +4,7 @@ require 'pg'
 describe 'User' do
   context 'create_account' do
     it "will create an account for a user with a username and a password" do
-      user = User.create_account("Wolverine", "xmen@gmail.com", "password")
+      user = User.create_account("Wolverine", "Xmen@gmail.com", "password")
     expect(user.username).to eq("Wolverine")
    end
   end
