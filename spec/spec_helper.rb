8bab169fc352319c0962f63a95da6952f6437ef4
Al <>
2018-12-04 10:35:20 +0000

    Add database_helper for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'database_helpers.rb'
 
 require 'capybara/rspec'
 require 'pg'
@@ -7,6 +8,10 @@ require 'pg'
 Capybara.app = Makersbnb
 
 RSpec.configure do |config|
+  config.before(:each) do
+    set_up_database
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
