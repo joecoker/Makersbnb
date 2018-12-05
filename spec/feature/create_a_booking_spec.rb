c17bb8f66d7fd0ef711036db789c0a983284bf1d
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-12-05 15:50:48 +0000

    adds front end html for booking request form

---
+++ spec/feature/create_a_booking_spec.rb
@@ -0,0 +1,10 @@
+feature 'User submits a booking request' do
+  scenario 'user makes a booking request on a space and can see all requests' do
+    test_login
+    click_link(DEFAULT_SPACE[:spacename])
+    fill_in('start_date', with: '2018-12-20')
+    fill_in('end_date', with: '2018-12-23')
+    click_button('Request booking')
+    expect(page).to have_content('Booking request for Pickle Place pending')
+  end
+end
